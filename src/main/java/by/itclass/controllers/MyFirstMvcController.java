4493c6a69307ae90f971b38270d113c857b04644
Test User <Test5User@yandex.by>
2024-07-31 21:08:43 +0300

    Initial MVC2

---
+++ src/main/java/by/itclass/controllers/MyFirstMvcController.java
@@ -0,0 +1,12 @@
+package by.itclass.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyFirstMvcController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/by/itclass/controllers/ParamsConroller.java
@@ -0,0 +1,4 @@
+package by.itclass.controllers;
+
+public class ParamsConroller {
+}
