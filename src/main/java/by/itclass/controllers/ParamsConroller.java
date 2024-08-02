10ccc2c327a7e2db9b3b763f070dc76acf6ea9cf
Test User <Test5User@yandex.by>
2024-08-02 19:39:53 +0300

    MVC4

--- src/main/java/by/itclass/controllers/ParamsConroller.java
+++ src/main/java/by/itclass/controllers/ParamsConroller.java
@@ -1,8 +1,13 @@
 package by.itclass.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ParamsConroller {
@@ -13,5 +18,56 @@ public class ParamsConroller {
         request.setAttribute("stringParam", param1);
         request.setAttribute("intParam", param2);
         return "main";
+    }
+
+    @GetMapping("/getParamsSpring")
+    public String getParamsSpring(
+            @RequestParam(name = "param1") String name,
+            @RequestParam(name = "param2") int age,
+            HttpServletRequest request) {
+        request.setAttribute("stringParam", name);
+        request.setAttribute("intParam", age);
+        return "main";
+    }
+
+    @GetMapping("/getParamsRequired")
+    public String getParamsRequired(
+            @RequestParam(name = "param1") String name,
+            @RequestParam(name = "param2", required = false) Integer age,
+            HttpServletRequest request) {
+        request.setAttribute("stringParam", name);
+        request.setAttribute("intParam", age);
+        return "main";
+    }
+
+    @GetMapping("/getParamsToSession")
+    public String getParamsToSession(
+            @RequestParam(name = "param1") String name,
+            @RequestParam(name = "param2") int age,
+            HttpSession session) {
+        session.setAttribute("stringParam", name);
+        session.setAttribute("intParam", age);
+        return "main";
+    }
+
+    @GetMapping("/getParamsFromPath/{param1}/{param2}")
+    public String getParamsFromPath(
+            @PathVariable(name = "param1") String name,
+            @PathVariable(name = "param2") int age,
+            Model model) {
+        model.addAttribute("stringParam", name);
+        model.addAttribute("intParam", age);
+        return "main";
+    }
+
+    @GetMapping("/getParamsToModelAndView")
+    public ModelAndView getParamsToModelAndView(
+            @RequestParam(name = "param1") String name,
+            @RequestParam(name = "param2") int age) {
+        var modelAndView = new ModelAndView();
+        modelAndView.setViewName("main");
+        modelAndView.addObject("stringParam", name);
+        modelAndView.addObject("intParam", age);
+        return modelAndView;
     }
 }
